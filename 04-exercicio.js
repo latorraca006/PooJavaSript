1b31c438170bfbc808cda3252cf62b5d96a80f82
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-12 10:30:02 -0400

    add exercicio classe POO

---
+++ 04-exercicio.js
@@ -0,0 +1,10 @@
+class Personagem {
+    // Construtor para inicializar os atributos
+    constructor(nome, idade, classe, hp, mp) {
+        this.nome = nome;    
+        this.idade = idade;   
+        this.classe = classe; 
+        this.hp = hp;         
+        this.mp = mp;       
+    }
+}
