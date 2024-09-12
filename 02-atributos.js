42585de7df90410e614b6be91d8548f93a20337b
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-12 08:40:53 -0400

    add atributos POO

---
+++ 02-atributos.js
@@ -0,0 +1,10 @@
+class Pessoa {
+    constructor(nome, idade, altura, peso) { // Atributos
+        // Inicialização dos atributos
+        this.nome = "Amanda Latorraca";
+        this.idade = 18;
+        this.altura = 1.67;
+        this.peso = 53;
+
+    }
+}
