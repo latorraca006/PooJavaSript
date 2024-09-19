717bbf63f3f0c5053f25002c7ee0401a2fb171cd
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-19 08:26:07 -0400

    Classe Estudante

--- 07-heranca.js
+++ 07-heranca.js
@@ -30,3 +30,13 @@ class Aluno extends Estudante {
         super(nome, idade, altura, peso, curso);
     }
 }
+
+// Criando objetos aluno, diretor e professor
+const aluno = new Estudante("Amanda", 18, 1.67, 54, "Progamação de Jogos Digitais");
+const diretor = new Diretor("Sergio", 45, 1.65, 65, 3000);
+const professor = new Professor("Wanderson", 40, 1.65, 65, 3000);
+
+// Mostrando no console  a saida
+console.log(aluno); // Saida: Estudante {nome: 'Amanda', idade: 18, altura:1.67, peso: 54, curso: 'Progamação de Jogos Digitais'}
+console.log(diretor); // Saida: Diretor {nome: 'Sergio', idade: 45, altura:1.65, peso: 65, salario: '3000'}
+console.log(professor); // Saida: professor {nome: 'Wanderson', idade: 40, altura:1.65, peso: 65, salario: '3000'}
